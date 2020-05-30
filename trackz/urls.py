98c76ca3397390aca6a2f3024b099fc0e4fc3a04
techrepublik <joshlorilla@gmail.com>
2020-05-30 18:07:45 +0800

    'ground_one'

--- trackz/urls.py
+++ trackz/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_profiles, name='get_profiles'),
-    path('get_profile/<int:pk>/', views.get_profile, name='get_profile'),
+    path('<int:pk>/', views.get_profile, name='get_profile'),
 ]
